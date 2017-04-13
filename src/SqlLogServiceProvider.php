e57d276ec17da840186834e03ab932809cfc57b9
Stas Karpov <gilbert_90@mail.ru>
2017-04-13 16:50:42 +0300

    Change log formatting

--- src/SqlLogServiceProvider.php
+++ src/SqlLogServiceProvider.php
@@ -3,6 +3,10 @@
 namespace Staskjs\LaravelUtils;
 
 use Illuminate\Support\ServiceProvider;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Monolog\Formatter\LineFormatter;
+use Illuminate\Support\Str;
 
 class SqlLogServiceProvider extends ServiceProvider
 {
@@ -14,15 +18,44 @@ class SqlLogServiceProvider extends ServiceProvider
     public function boot()
     {
         if (\Config::get('app.debug')) {
+            $logger = $this->getLogger();
             $url = !empty($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '';
-            \Log::info("-------------------");
+            $method = !empty($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : '';
             if (!empty($url)) {
-                \Log::info("--------- $url");
+                $logger->info('');
+                $date = \Carbon\Carbon::now();
+                $logger->info("--------- $date --------- $method $url");
             }
-            \Log::info("-------------------");
-            \DB::listen(function($query) {
-                \Log::info("({$query->time} ms) {$query->sql}");
+            $logger->info('');
+            \DB::listen(function($query) use ($logger) {
+                $bindings = $query->bindings;
+                foreach ($bindings as &$binding) {
+                    $binding = is_numeric($binding) ? $binding : "'$binding'";
+                }
+
+                $sqlWords = [
+                    'select', 'from', 'where', 'order by', 'group by', 'limit', 'offset', 'and', 'or', 'not',
+                    'set', 'update', 'delete', 'insert', 'into', 'values', 'join', 'left', 'inner', 'outer', 'right', 'having',
+                    'count', 'max', 'min', 'avg', 'sum', 'like', 'between', 'asc', 'desc',
+                ];
+                $sql = Str::replaceArray('?', $bindings, $query->sql);
+                foreach ($sqlWords as $word) {
+                    $sql = preg_replace("/\b{$word}\b/", strtoupper($word), $sql);
+                }
+                $sql = str_replace('`', '', $sql);
+                $logger->info("sql: ({$query->time} ms) {$sql};");
             });
         }
+    }
+
+    protected function getLogger() {
+        $logStreamHandler = new StreamHandler(storage_path('logs/laravel.log'), 'info');
+        $logFormat = "%message%\n";
+        $formatter = new LineFormatter($logFormat);
+        $logStreamHandler->setFormatter($formatter);
+        $logger = new Logger('sql');
+        $logger->pushHandler($logStreamHandler);
+
+        return $logger;
     }
 }
