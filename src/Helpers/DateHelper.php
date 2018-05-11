00644e9e41d52496ffce258c415cb4c7ca6b273b
Stas Karpov <staskjs@mail.ru>
2018-05-11 13:13:15 +0300

    Add more date formats to date helper

--- src/Helpers/DateHelper.php
+++ src/Helpers/DateHelper.php
@@ -9,4 +9,20 @@ class DateHelper
     public static function formatDate($date, $format = DB_DATE_FORMAT) {
         return Carbon::parse($date)->format($format);
     }
+
+    public static function dbDate($date) {
+        return self::formatDate($date, 'Y-m-d');
+    }
+
+    public static function dbDatetime($date) {
+        return self::formatDate($date, 'Y-m-d H:i:s');
+    }
+
+    public static function ukDate($date) {
+        return self::formatDate($date, 'd/m/Y');
+    }
+
+    public static function ukDatetime($date) {
+        return self::formatDate($date, 'd/m/Y H:i:s');
+    }
 }
